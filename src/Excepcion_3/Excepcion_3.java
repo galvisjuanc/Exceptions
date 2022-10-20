d840ca452fc4d43aa53832158672866dfe8ec45b
Juan <galvisjuanc@gmail.com>
2022-10-20 07:50:48 -0500

    Try Catch para Excepciones Verificadas

--- src/Excepcion_3/Excepcion_3.java
+++ src/Excepcion_3/Excepcion_3.java
@@ -1,22 +1,36 @@
 package Excepcion_3;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
+/**
+ * Try Catch para Excepciones Verificadas
+ * */
 public class Excepcion_3 {
 
-    public void leerArchivo() throws FileNotFoundException {
-        File archivo = new File("C:\\Users\\galvi\\Documents\\Excepciones\\TextoPrueba.txt");
+    public void leerArchivo() throws FileNotFoundException, IOException {
+        File archivo = new File("C:\\Users\\galvi\\Documents\\Excepciones\\TextoPrueba1.txt");
         FileReader fr = new FileReader(archivo);
+        BufferedReader bf = new BufferedReader(fr);
+        String linea;
+
+        while((linea = bf.readLine()) != null){
+            System.out.println(linea);
+        }
     }
 
-    public void leerArchivo2() throws IOException {
-        leerArchivo();
+    public void leerArchivo2(){
+        try {
+            leerArchivo();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error Archivo --> "+e);
+        } catch (IOException er){
+            System.out.println("Error General --> "+er);
+        }
+        System.out.println("\nPrograma terminado");
     }
 
-    public static void main(String[] args) {
-        //Hi
+    public static void main(String[] args) throws IOException {
+        Excepcion_3 exc = new Excepcion_3();
+        exc.leerArchivo2();
     }
 }
